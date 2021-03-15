f3025ca1faa72953d5b4644568870953f7f87496
g1gzer <38072226+g1gzer@users.noreply.github.com>
2021-03-15 20:39:36 +0300

    feat: completed the task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min (arr) {
+  return arr === undefined || arr.length == 0 ? 0 : Math.min( ...arr );
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max (arr) {
+    return arr === undefined || arr.length == 0 ? 0 : Math.max( ...arr );
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg (arr) {
+    return arr === undefined || arr.length == 0 ? 0 : arr.reduce((a, b) => (a + b)) / arr.length;
 }
